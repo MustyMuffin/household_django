590848a25cb57e889bbb2fddf872e79481852127
MustyMuffin <mustymuffin@icloud.com>
2024-11-14 22:19:43 -0700

    Added 'wage' variable to Chore model
    Also began work on home page.
    
    Todo
     - Figure out best way to track wages earned by laborer. Possibly a new model/db table, or possibly logic when the chore entry is logged. Or a combination of the two.
     - Finish homepage and display aforementioned earned wages
     - Figure out how to dump earned totals on payouts

--- chores/models.py
+++ chores/models.py
@@ -6,7 +6,7 @@ class Chore(models.Model):
     """Table mapping admin selected chores to variables"""
     text = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
-    # wage = models.DecimalField(max_digits=5, decimal_places=2)
+    wage = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -22,3 +22,8 @@ class ChoreEntry(models.Model):
     class Meta:
         verbose_name_plural = 'chore_entries'
         # permissions = (("can_log_chore", "Can Log Chore"),)
+
+# class EarnedWage(models.Model):
+#     """For tracking the wages earned"""
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     earned =
